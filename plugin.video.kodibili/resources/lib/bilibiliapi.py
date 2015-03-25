c9273860764e2f3ff354e9aea9358fef746a92f9
Yang Wang <yesh2333@126.com>
2015-03-26 01:20:19 +0800

    fix chinese issue & bilibili -> bilibiliapi

---
+++ plugin.video.kodibili/resources/lib/bilibiliapi.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'yesh233'
+
+import requests
+import hashlib
+import json
+import urllib
+
+
+class BiliBiliAPI(object):
+    __app_key = '85eb6835b0a1034e'
+    __secret_key = '2ad42749773c441109bdc0191257a664'
+    __ver = '0.98.72'
+    __ua = 'Biligrab / ' + __ver + ' (cnbeining@gmail.com)'
+    __headers = {'User-Agent': __ua}
+    __api_url = 'http://api.bilibili.com/'
+
+    @staticmethod
+    def __string_sign(string):
+        return unicode(hashlib.md5(unicode(string)).hexdigest())
+
+    @staticmethod
+    def __calc_sign(params):
+        return BiliBiliAPI.__string_sign('&'.join([unicode(name) + '=' +
+                                                urllib.quote(unicode(params[name]).encode('utf-8'))
+                                                for name in sorted(params)]) +
+                                      BiliBiliAPI.__secret_key)
+
+    @staticmethod
+    def __calc_params(params):
+        params['appkey'] = BiliBiliAPI.__app_key
+        params['sign'] = BiliBiliAPI.__calc_sign(params)
+        return params
+
+    @staticmethod
+    def api(url, params=dict()):
+        return json.loads(requests.get(BiliBiliAPI.__api_url+url, params=BiliBiliAPI.__calc_params(params),
+                          headers=BiliBiliAPI.__headers).text)
+
--- plugin.video.kodibili/resources/lib/test_Bilibili.py
+++ plugin.video.kodibili/resources/lib/test_Bilibili.py
@@ -3,21 +3,20 @@
 __author__ = 'yesh233'
 
 import unittest
-from bilibili import Bilibili
+from bilibiliapi import BiliBiliAPI
 
 
 class BilibiliTestCase(unittest.TestCase):
-    #
-    # def test_api_view(self):
-    #     self.assertEquals(self.__bilibili.api('view', {'id': 312943})['title'], u'【BDRip720P】翼·年代记 第一季全【华盟】')
-    #
-    # def test_api_index(self):
-    #     self.assertEquals(self.__bilibili.api('index').keys(),
-    #                       [u'type36', u'type11', u'type119', u'type3', u'type5',
-    #                        u'type4', u'type23', u'type1', u'type13', u'type129'])
+    def test_api_view(self):
+        self.assertEquals(BiliBiliAPI.api('view', {'id': 312943})['title'], u'【BDRip720P】翼·年代记 第一季全【华盟】')
+
+    def test_api_index(self):
+        self.assertEquals(BiliBiliAPI.api('index').keys(),
+                          [u'type36', u'type11', u'type119', u'type3', u'type5',
+                           u'type4', u'type23', u'type1', u'type13', u'type129'])
 
     def test_api_search(self):
-        print Bilibili.api('search', {'keyword': u'中文'})
+        print BiliBiliAPI.api('search', {'keyword': u'洛天依'})['result'][0].keys()
 
     def test_api_list(self):
-        print Bilibili.api('list', {'tid': 33})
+        print BiliBiliAPI.api('list', {'tid': 3})['results']
